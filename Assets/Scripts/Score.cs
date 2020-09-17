aa15220aaaab50fc54746edce4d8bde581671018
keishin morita <mk.nba.love@gmail.com>
2020-09-17 10:02:29 +0900

    [add]add date
    コンテンツ制作データ作成

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,33 @@
+﻿using RunGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// スコアUIを制御します。
+public class Score : MonoBehaviour
+{
+    // 値の表示テキストを指定します。
+    [SerializeField]
+    private Text valueText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateValueText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateValueText();
+    }
+
+    // 値の表示を更新
+    private void UpdateValueText()
+    {
+        var score = StageScene.Instance.Score;
+        // 10進数6桁でゼロキャスト
+        valueText.text = score.ToString("D06");
+    }
+}
